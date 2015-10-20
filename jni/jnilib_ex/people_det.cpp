d9f50f7d2421a69634d0efb9f70f65673da04100
TzuTa Lin <tzu.ta.lin@gmail.com>
2015-10-20 17:31:02 +0800

    refactor people_det

--- jni/jnilib_ex/people_det.cpp
+++ jni/jnilib_ex/people_det.cpp
@@ -28,7 +28,6 @@
 #define  LOGW(...)  __android_log_print(ANDROID_LOG_WARN,LOG_TAG, __VA_ARGS__)
 #define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG, __VA_ARGS__)
 
-
 class OpencvHOGDetctor {
 public:
     OpencvHOGDetctor() {
@@ -38,77 +37,118 @@ public:
         LOGD("det path %s", path.c_str());
         cv::Mat src_img = cv::imread(path, 1);
         if (src_img.empty())
-            return -1;
+            return 0;
 
         cv::HOGDescriptor hog;
         hog.setSVMDetector(cv::HOGDescriptor::getDefaultPeopleDetector());
         std::vector<cv::Rect> found, found_filtered;
-        hog.detectMultiScale(src_img, found, 0, cv::Size(8,8), cv::Size(32,32), 1.05, 2);
+        hog.detectMultiScale(src_img, found, 0, cv::Size(8, 8),
+                cv::Size(32, 32), 1.05, 2);
         size_t i, j;
-        for (i=0; i<found.size(); i++)
-        {
+        for (i = 0; i < found.size(); i++) {
             cv::Rect r = found[i];
-            for (j=0; j<found.size(); j++)
-                if (j!=i && (r & found[j])==r)
+            for (j = 0; j < found.size(); j++)
+                if (j != i && (r & found[j]) == r)
                     break;
-            if (j==found.size())
+            if (j == found.size())
                 found_filtered.push_back(r);
         }
 
-        for (i=0; i<found_filtered.size(); i++)
-        {
+        for (i = 0; i < found_filtered.size(); i++) {
             cv::Rect r = found_filtered[i];
-            r.x += cvRound(r.width*0.1);
-            r.width = cvRound(r.width*0.8);
-            r.y += cvRound(r.height*0.06);
-            r.height = cvRound(r.height*0.9);
-            cv::rectangle(src_img, r.tl(), r.br(), cv::Scalar(0,255,0), 2);
+            r.x += cvRound(r.width * 0.1);
+            r.width = cvRound(r.width * 0.8);
+            r.y += cvRound(r.height * 0.06);
+            r.height = cvRound(r.height * 0.9);
+            cv::rectangle(src_img, r.tl(), r.br(), cv::Scalar(0, 255, 0), 2);
         }
         mResultMat = src_img;
-        cv::imwrite( "/sdcard/testret.jpg", mResultMat);
+        cv::imwrite(path, mResultMat);
         LOGD("det ends");
-        return 0;
+        mRets = found_filtered;
+        return found_filtered.size();
     }
 
     inline cv::Mat& getResultMat() {
         return mResultMat;
     }
+
+    inline std::vector<cv::Rect> getResult() {
+        return mRets;
+    }
+
 private:
     cv::Mat mResultMat;
+    std::vector<cv::Rect> mRets;
 };
 
-class DLibHOGFaceDetector{
+class DLibHOGFaceDetector {
 public:
     DLibHOGFaceDetector() {
-  
-    }    
-    
-    inline void det(std::string path) {
-        dlib::frontal_face_detector detector = dlib::get_frontal_face_detector();
-        dlib::array2d<dlib::rgb_pixel> img;        
-        load_image(img, path);        
-        dlib::pyramid_up(img);        
-        std::vector<dlib::rectangle> dets = detector(img);
-        LOGD("Dlib HOG face det size : %d", dets.size());
+
     }
+
+    inline int det(std::string path) {
+        dlib::frontal_face_detector detector =
+                dlib::get_frontal_face_detector();
+        dlib::array2d<dlib::rgb_pixel> img;
+        load_image(img, path);
+        //dlib::pyramid_up(img);
+        mRets = detector(img);
+        LOGD("Dlib HOG face det size : %d", mRets.size());
+
+        // Draw on the input for test
+        int i = 0;
+        cv::Mat src_img = cv::imread(path, 1);
+        for (i = 0; i < mRets.size(); i++) {
+            dlib::rectangle dlibrect = mRets[i];
+            cv::Rect r(dlibrect.left(), dlibrect.top(), dlibrect.width(),
+                    dlibrect.height());
+            r.x += cvRound(r.width * 0.1);
+            r.width = cvRound(r.width * 0.8);
+            r.y += cvRound(r.height * 0.06);
+            r.height = cvRound(r.height * 0.9);
+            cv::rectangle(src_img, r.tl(), r.br(), cv::Scalar(0, 255, 0), 2);
+        }
+        cv::imwrite(path, src_img);
+
+        return mRets.size();
+    }
+
+    inline std::vector<dlib::rectangle> getResult() {
+        return mRets;
+    }
+
 private:
     std::string mModel;
+    std::vector<dlib::rectangle> mRets;
 };
+
+OpencvHOGDetctor* mOpencvHOGDetctor;
+DLibHOGFaceDetector* mDlibFaceDetector;
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+struct VisionDetRetOffsets {
+    jfieldID label;
+    jfieldID confidence;
+    jfieldID left;
+    jfieldID top;
+    jfieldID right;
+    jfieldID bottom;
+} gVisionDetRetOffsets;
+
 // ========================================================
 // JNI Mapping Methods
 // ========================================================
-jint JNIEXPORT JNICALL JNI_OnLoad(JavaVM *vm, void *reserved)
-{
+jint JNIEXPORT JNICALL JNI_OnLoad(JavaVM *vm, void *reserved) {
     LOGE("JNI On Load");
     JNIEnv* env = NULL;
     jint result = -1;
 
-    if (vm->GetEnv((void**)&env, JNI_VERSION_1_6) != JNI_OK) {
+    if (vm->GetEnv((void**) &env, JNI_VERSION_1_6) != JNI_OK) {
         LOGE("GetEnv failed!");
         return result;
     }
@@ -117,14 +157,23 @@ jint JNIEXPORT JNICALL JNI_OnLoad(JavaVM *vm, void *reserved)
 }
 
 void JNIEXPORT
-JNICALL Java_com_tzutalin_dlib_PeopleDet_jniNativeClassInit(
-    JNIEnv *_env, jclass _this) {
-     LOGD("JniNativeClassIni");
+JNICALL Java_com_tzutalin_dlib_PeopleDet_jniNativeClassInit(JNIEnv *_env,
+        jclass _this) {
+    jclass detRetClass = _env->FindClass("com/tzutalin/dlib/VisionDetRet");
+    gVisionDetRetOffsets.label = _env->GetFieldID(detRetClass, "mLabel",
+            "java/lang/String");
+    gVisionDetRetOffsets.confidence = _env->GetFieldID(detRetClass,
+            "mConfidence", "F");
+    gVisionDetRetOffsets.left = _env->GetFieldID(detRetClass, "mLeft", "I");
+    gVisionDetRetOffsets.top = _env->GetFieldID(detRetClass, "mTop", "I");
+    gVisionDetRetOffsets.right = _env->GetFieldID(detRetClass, "mRight", "I");
+    gVisionDetRetOffsets.bottom = _env->GetFieldID(detRetClass, "mBottom", "I");
+    LOGD("JniNativeClassIni Success");
 }
 
 jint JNIEXPORT JNICALL
 Java_com_tzutalin_dlib_PeopleDet_jniOpencvHOGDetect(
-    JNIEnv* env, jobject thiz, jstring imgPath)
+        JNIEnv* env, jobject thiz, jstring imgPath)
 {
     LOGD("com_tzutalin_dlib_PeopleDet jniOpencvHOGDetect");
     const char *img_path = env->GetStringUTFChars(imgPath, 0);
@@ -134,17 +183,52 @@ Java_com_tzutalin_dlib_PeopleDet_jniOpencvHOGDetect(
     return 0;
 }
 
+jint JNIEXPORT JNICALL
+Java_com_tzutalin_dlib_PeopleDet_jniGetOpecvHOGRet(JNIEnv* env,
+        jobject thiz, jobject detRet, jint index)
+{
+    if (mOpencvHOGDetctor) {
+        cv::Rect rect = mOpencvHOGDetctor->getResult()[index];
+        env->SetIntField(detRet, gVisionDetRetOffsets.left, rect.x);
+        env->SetIntField(detRet, gVisionDetRetOffsets.top, rect.y);
+        env->SetIntField(detRet, gVisionDetRetOffsets.right, rect.x + rect.width);
+        env->SetIntField(detRet, gVisionDetRetOffsets.bottom, rect.y + rect.height);
+        env->SetFloatField(detRet, gVisionDetRetOffsets.confidence, 0);
+        jstring jstr=(jstring)(env->NewStringUTF("person"));
+        env->SetObjectField(detRet, gVisionDetRetOffsets.label, (jobject)jstr);
+        return 0;
+    }
+    return -1;
+}
 
 jint JNIEXPORT JNICALL
 Java_com_tzutalin_dlib_PeopleDet_jniDLibHOGDetect(
-    JNIEnv* env, jobject thiz, jstring imgPath)
+        JNIEnv* env, jobject thiz, jstring imgPath, jobject detRet)
 {
     LOGD("com_tzutalin_dlib_PeopleDet jniDLibHOGDetect");
     const char *img_path = env->GetStringUTFChars(imgPath, 0);
-    DLibHOGFaceDetector hogdet;    
-    hogdet.det(std::string(img_path));
+    mDlibFaceDetector = new DLibHOGFaceDetector();
+    int size = mDlibFaceDetector->det(std::string(img_path));
     env->ReleaseStringUTFChars(imgPath, img_path);
-    return 0;
+    return size;
+}
+
+jint JNIEXPORT JNICALL
+Java_com_tzutalin_dlib_PeopleDet_jniGetDLibRet(JNIEnv* env,
+        jobject thiz, jobject detRet, jint index)
+{
+    if (mDlibFaceDetector) {
+        dlib::rectangle rect = mDlibFaceDetector->getResult()[index];
+        env->SetIntField(detRet, gVisionDetRetOffsets.left, rect.left());
+        env->SetIntField(detRet, gVisionDetRetOffsets.top, rect.top());
+        env->SetIntField(detRet, gVisionDetRetOffsets.right, rect.right());
+        env->SetIntField(detRet, gVisionDetRetOffsets.bottom, rect.bottom());
+        env->SetFloatField(detRet, gVisionDetRetOffsets.confidence, 0);
+        jstring jstr=(jstring)(env->NewStringUTF("face"));
+        env->SetObjectField(detRet, gVisionDetRetOffsets.label, (jobject)jstr);
+        return 0;
+    }
+    return -1;
 }
 
 #ifdef __cplusplus
