0c8ca9215b467c389f501de761433165153950ce
tzutalin <tzu.ta.lin@gmail.com>
2015-11-25 22:52:16 +0800

    rm *sh

--- cleanbuild.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-ndk-build clean
-rm -rf libs
-rm -rf obj
-adb shell rm ./data/local/tmp/svm_ex
-adb shell rm ./data/local/tmp/max_cost_assignment_ex
-adb shell rm ./data/local/tmp/svr_ex
-adb shell rm ./data/local/tmp/TestSelectiveSearch
-adb shell rm ./data/local/tmp/face_landmark
