d2deee26d65f75206931147aa227974d3514a652
tzutalin <tzu.ta.lin@gmail.com>
2016-06-21 14:21:20 +0800

    workaround for travis:Limited building log

--- travis-ci/setupNDK.sh
+++ travis-ci/setupNDK.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 wget https://dl.google.com/android/ndk/android-ndk-r10e-linux-x86_64.bin
 chmod +x android-ndk-r10e-linux-x86_64.bin
-./android-ndk-r10e-linux-x86_64.bin 2>/dev/null
+./android-ndk-r10e-linux-x86_64.bin 1> /dev/null 2>&1
 mv android-ndk-r10e android-ndk
